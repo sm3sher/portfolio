75337f66fdbf22587639be54e4fa1fd9ab336631
sm3sher <roman.jum99@gmail.com>
2024-11-11 13:00:24 +0100

    style: adjust colors for lightmode

--- app/(portfolio)/layout.tsx
+++ app/(portfolio)/layout.tsx
@@ -33,16 +33,22 @@ export default function Layout({
   return (
     <div>
       <nav className="mx-auto flex max-w-screen-xl items-center justify-between px-6 py-6">
-        <div>Language toggle</div>
-        <div className="flex space-x-1 rounded-2xl border border-white/20 p-1">
+        <button>EN/DE</button>
+        <div className="flex space-x-1 rounded-2xl border border-black/20 p-1 dark:border-white/20">
           {navbarItems.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className={`flex items-center rounded-xl border border-transparent px-3 py-0.5 font-medium transition duration-300 ${pathname === item.href ? 'border-white/20 bg-neutral-600/60' : 'hover:border-white/10 hover:bg-neutral-600/15'}`}
+              className={`flex items-center rounded-xl border border-transparent font-medium transition duration-300 ${
+                pathname === item.href
+                  ? 'border-black/35 bg-neutral-400/30 dark:border-white/20 dark:bg-neutral-600/60'
+                  : 'hover:border-black/15 hover:bg-neutral-400/10 dark:hover:border-white/15 dark:hover:bg-neutral-600/20'
+              }`}
             >
-              <span className="mb-0.5 mr-2">{item.icon}</span>
-              {item.label}
+              <span className="px-3 py-1">{item.icon}</span>
+              <span className="hidden pr-3 leading-7 sm:block">
+                {item.label}
+              </span>
             </Link>
           ))}
         </div>
