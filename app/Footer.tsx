88b7386579386c735639cbb5f06b7e2c6fa2529f
sm3sher <roman.jum99@gmail.com>
2024-11-11 17:31:47 +0100

    style: extract footer color to css var

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="m-4 mt-auto rounded-lg">
+    <footer className="m-4 mt-auto rounded-lg text-[--footer]">
       <div className="mx-auto max-w-screen-xl p-4 sm:flex sm:items-center sm:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm sm:text-center">
           @ {new Date().getFullYear()} - Roman Jumatov
         </span>
-        <ul className="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400">
+        <ul className="mt-3 flex flex-wrap items-center text-sm font-medium sm:mt-0">
           <li>
             <a
               className="me-4 hover:underline hover:underline-offset-4 sm:me-6"
@@ -28,7 +28,7 @@ export default function Footer() {
           </li>
           <li>
             <a
-              className="me-4 hover:underline hover:underline-offset-4 sm:me-6"
+              className="hover:underline hover:underline-offset-4"
               href="mailto:roman.jum99@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,4 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: 'selector',
-  theme: {
-    extend: {
-      colors: {
-        foreground: 'var(--foreground)',
-      },
-    },
-  },
-  plugins: [],
 } satisfies Config;
