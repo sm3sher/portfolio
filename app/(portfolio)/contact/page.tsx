b569a6880cdfcc3c035e074cc75da38e8b2cdb6b
sm3sher <roman.jum99@gmail.com>
2024-11-13 13:37:48 +0100

    feat: add contact markup

--- app/(portfolio)/contact/page.tsx
+++ app/(portfolio)/contact/page.tsx
@@ -1,3 +1,18 @@
 export default function Page() {
-  return <h1 className="text-5xl font-extrabold">Contact</h1>;
+  return (
+    <>
+      <h1 className="text-5xl font-extrabold">👋 Drop me a message!</h1>
+      <p className="my-3">
+        <span className="rounded-md border border-gray-500 bg-gray-200 bg-opacity-80 p-0.5 font-[family-name:var(--font-geist-mono)] text-lg font-medium dark:border-gray-500 dark:bg-gray-900">
+          #FeelFreeToUseFirstNames
+        </span>
+      </p>
+      <p className="text-2xl">
+        You can send me a message here. If you’re proposing a collaboration on a
+        specific project, please describe the project’s goals and the tasks as
+        clearly as possible. The more details you provide, the better I can
+        assess if the collaboration would be a good fit!
+      </p>
+    </>
+  );
 }
