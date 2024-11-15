16387612e65c23e7aeacb9a382fd022345bbaaf8
sm3sher <roman.jum99@gmail.com>
2024-11-15 10:00:36 +0100

    style: add hover bg to theme toggle

--- app/(portfolio)/ThemeToggle.tsx
+++ app/(portfolio)/ThemeToggle.tsx
@@ -23,7 +23,7 @@ export default function ThemeToggle() {
   ) : (
     <button
       onClick={toggleTheme}
-      className="rounded-full p-1 duration-300 hover:scale-110"
+      className="rounded-full p-1 duration-300 hover:scale-110 hover:bg-black hover:bg-opacity-10 dark:hover:bg-white dark:hover:bg-opacity-20"
     >
       {isDark ? <Sun03Icon /> : <Moon02Icon />}
     </button>
