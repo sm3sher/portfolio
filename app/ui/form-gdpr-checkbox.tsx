50224150fa101777d1583552340954571d84f781
sm3sher <roman.jum99@gmail.com>
2024-11-29 12:02:34 +0100

    feat: display error message for checkbox

--- app/ui/form-gdpr-checkbox.tsx
+++ app/ui/form-gdpr-checkbox.tsx
@@ -5,7 +5,7 @@ import {
   UseFormRegister,
 } from 'react-hook-form';
 import Link from 'next/link';
-import { Tick02Icon } from 'hugeicons-react';
+import { Alert01Icon, Tick02Icon } from 'hugeicons-react';
 
 type Props<T extends FieldValues> = {
   register: UseFormRegister<T>;
@@ -18,9 +18,7 @@ export default function FormGdprCheckbox<T extends FieldValues>({
   name,
   errors,
 }: Props<T>) {
-  // TODO: handle errorMessage
   const errorMessage = errors[name]?.message && String(errors[name]?.message);
-  console.error(errorMessage);
 
   return (
     <div className="inline-flex items-center">
@@ -31,15 +29,19 @@ export default function FormGdprCheckbox<T extends FieldValues>({
         <input
           {...register(name)}
           type="checkbox"
-          className="base-border peer h-5 w-5 cursor-pointer appearance-none rounded transition-colors duration-200 checked:bg-[--color]"
+          className={`backdrop base-border peer h-5 w-5 cursor-pointer appearance-none rounded transition-colors duration-200 checked:bg-[--color] ${
+            errorMessage &&
+            'border-[--error-color] bg-[--error-background] outline-[--error-color]'
+          }`}
           id="check-with-link"
+          aria-describedby={`${String(name)}-error`}
         />
         <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[--background-color] opacity-0 peer-checked:opacity-100">
           <Tick02Icon size={16} strokeWidth={3} />
         </span>
       </label>
       <label
-        className="ml-2 cursor-pointer select-none text-sm text-[--footer]"
+        className={`ml-2 cursor-pointer select-none text-sm text-[--footer] ${errorMessage && 'text-[--error-color]'}`}
         htmlFor="check-with-link"
       >
         <p>
@@ -53,6 +55,16 @@ export default function FormGdprCheckbox<T extends FieldValues>({
           . My provided data will be stored for the purpose of creating and
           processing my request.
         </p>
+        {errorMessage && (
+          <div
+            id={`${String(name)}-error`}
+            aria-live="polite"
+            className="pointer-events-none absolute z-10 ml-[-1.75rem] mt-2 flex items-center space-x-2 rounded-lg border border-[--error-color] bg-[--error-background] px-3 py-2 text-sm text-[--color]"
+          >
+            <Alert01Icon />
+            <span>{errorMessage}</span>
+          </div>
+        )}
       </label>
     </div>
   );
