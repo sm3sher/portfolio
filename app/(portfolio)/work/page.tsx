6a1e913bf2922ba497177ea51ad4a083130d6710
sm3sher <roman.jum99@gmail.com>
2024-12-08 17:46:36 +0100

    feat: add work timeline

--- app/(portfolio)/work/page.tsx
+++ app/(portfolio)/work/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import Timeline from '@/app/ui/timeline';
+import { timelineData } from '@/app/lib/timeline-data';
 
 export const metadata: Metadata = {
   title: 'Roman Jumatov - Work',
@@ -7,5 +9,21 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <h1>Work</h1>;
+  return (
+    <div className="flex flex-col items-center justify-between gap-8 md:h-full md:flex-row md:items-center lg:gap-12">
+      <div className="flex flex-col justify-center space-y-6 md:w-5/12">
+        <h1 className="text-center md:text-left">My Work.</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet.
+        </p>
+      </div>
+      <div className="md:w-7/12">
+        <Timeline data={timelineData} />
+      </div>
+    </div>
+  );
 }
