4f854446eb106f2820b1abec45197e1681302b54
sm3sher <roman.jum99@gmail.com>
2024-12-09 17:21:53 +0100

    feat: add my work credo to work page

--- app/(portfolio)/work/page.tsx
+++ app/(portfolio)/work/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import Timeline from '@/app/ui/timeline';
 import { timelineData } from '@/app/lib/timeline-data';
+import Section from '@/app/ui/section';
+import Link from 'next/link';
+import Button from '@/app/ui/button/button';
 
 export const metadata: Metadata = {
   title: 'Roman Jumatov - Work',
@@ -10,18 +13,38 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-between gap-8 md:h-full md:flex-row md:items-center lg:gap-12">
-      <div className="flex flex-col justify-center space-y-6 md:w-5/12">
-        <h1 className="text-center md:text-left">My Work.</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet.
+    <div className="flex flex-col items-center justify-between gap-8 md:h-full md:flex-row md:items-center lg:gap-16">
+      <div className="flex flex-col items-center justify-center space-y-6 md:w-1/2">
+        <h1 className="w-full text-center md:text-left">My Work.</h1>
+        <p className="text-center md:text-left">
+          I create solutions that combine modern design and efficient
+          functionality. My concept of
+          <strong> &#34;less is more&#34;</strong> ensures that every project is
+          clean, impactful, and meaningful.
         </p>
+        <Section title="What I Offer">
+          <p>
+            Tailored design, development, and consulting services to bring your
+            ideas to life.
+          </p>
+        </Section>
+        <Section title="What I Do">
+          <p>
+            I focus on building elegant, user-friendly websites and applications
+            that align with your goals.
+          </p>
+        </Section>
+        <Section title="Be Part of My Journey">
+          <p>
+            Let’s create something amazing together. Reach out to discuss your
+            next big project.
+          </p>
+        </Section>
+        <Link href="/contact" tabIndex={-1}>
+          <Button>Contact Me</Button>
+        </Link>
       </div>
-      <div className="md:w-7/12">
+      <div className="md:w-1/2">
         <Timeline data={timelineData} />
       </div>
     </div>
