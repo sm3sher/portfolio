d3758f1a5fe1c38971a76358073a1823c2515ec9
sm3sher <roman.jum99@gmail.com>
2024-12-15 18:36:51 +0100

    style: make 404 page an astro that's lost in space

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,30 @@
-import { redirect } from 'next/navigation';
+import Astronaut from '@/app/ui/astronaut';
+import Link from 'next/link';
+import Button from '@/app/ui/button/button';
 
 export default function NotFound() {
-  redirect('/');
+  return (
+    <div className="mx-auto w-full max-w-screen-xl overflow-x-hidden p-6 pt-12">
+      <div className="flex flex-col-reverse items-center gap-6 md:h-full md:flex-row lg:gap-16">
+        <div className="md:w-1/2">
+          <Astronaut />
+        </div>
+        <div className="relative max-w-2xl space-y-6 md:w-1/2">
+          <h1 className="text-center md:text-left">Lost in Space?</h1>
+          <p className="text-center md:text-left">
+            Uh oh! It seems like you’ve drifted off course. Don’t worry, we’ll
+            help you navigate back to Earth.
+          </p>
+          <div className="flex justify-center space-x-3 md:justify-start">
+            <Link href="/" tabIndex={-1}>
+              <Button>Take Me Home</Button>
+            </Link>
+            <Link href="/contact" tabIndex={-1}>
+              <Button>Contact Human</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,4 +3,11 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: 'selector',
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+      },
+    },
+  },
 } satisfies Config;
