8038af40ebb5d550ed764180295fc57a393a2844
sm3sher <roman.jum99@gmail.com>
2025-01-01 18:45:45 +0100

    style: animate scroll to top button

--- app/ui/button/scroll-to-top.tsx
+++ app/ui/button/scroll-to-top.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { ArrowUp01Icon } from 'hugeicons-react';
+import { motion } from 'motion/react';
 
 export default function ScrollToTop() {
   const [showScrollButton, setShowScrollButton] = useState(false);
@@ -20,14 +21,15 @@ export default function ScrollToTop() {
   };
 
   return (
-    showScrollButton && (
-      <button
-        onClick={scrollToTop}
-        className="base-border fixed bottom-4 right-4 z-30 flex items-center justify-center rounded-full p-2 xl:bottom-8 xl:right-10"
-        aria-label="Scroll to top"
-      >
-        <ArrowUp01Icon className="relative bottom-0.5 text-[--secondary]" />
-      </button>
-    )
+    <motion.button
+      initial={{ opacity: 0, y: 50 }}
+      animate={showScrollButton ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+      transition={{ duration: 0.5 }}
+      onClick={scrollToTop}
+      className="base-border fixed bottom-4 right-4 z-30 flex items-center justify-center rounded-full p-2 xl:bottom-8 xl:right-10"
+      aria-label="Scroll to top"
+    >
+      <ArrowUp01Icon className="relative bottom-0.5 text-[--secondary]" />
+    </motion.button>
   );
 }
