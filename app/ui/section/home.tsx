e0573a290b37913acccae3c0b7c9018cc6964dd9
Roman Jumatov <roman.jum99@gmail.com>
2025-01-13 01:37:30 +0100

    style: add drop-shadow to main image

--- app/ui/section/home.tsx
+++ app/ui/section/home.tsx
@@ -27,7 +27,7 @@ export default async function Home({ locale }: Props) {
             duration={0.5}
           >
             <Image
-              className="absolute -right-32 bottom-0 max-h-[90%] w-auto object-contain brightness-110 hover:scale-105 sm:-right-20 md:-right-12 lg:right-0 dark:brightness-90"
+              className="absolute -right-32 bottom-0 max-h-[90%] w-auto object-contain brightness-110 drop-shadow-[0_0_10px_rgba(0,0,0,0.5)] hover:scale-105 sm:-right-20 md:-right-12 lg:right-0 dark:brightness-95 dark:drop-shadow-[0_0_5px_rgba(255,255,255,0.75)]"
               src={home.image.url}
               width={home.image.width}
               height={home.image.height}
