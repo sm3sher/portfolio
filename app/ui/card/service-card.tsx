0d85235175cf957d9ce2a3fbc93dd64bbb7539e7
Roman Jumatov <roman.jum99@gmail.com>
2025-01-13 15:00:50 +0100

    style: adjust light mode colors to be more subtle and soft

--- app/ui/card/service-card.tsx
+++ app/ui/card/service-card.tsx
@@ -13,7 +13,7 @@ export default function ServiceCard({ icon, title, tags, description }: Props) {
       <div className="absolute -left-1/4 top-1/4 h-36 w-[150%] animate-spin-slow bg-[--highlight] opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-80" />
       <div className="absolute inset-1 rounded-xl bg-[--banner-color]" />
       <div className="relative">
-        <div className="transition-colors duration-300 ease-in-out group-hover:text-[--highlight-subtle]">
+        <div className="transition-colors duration-300 ease-in-out group-hover:text-[--highlight]">
           {icon}
           <h5 className="mb-4 mt-3 md:mb-6 md:mt-4 lg:mb-8 lg:mt-6">{title}</h5>
         </div>
