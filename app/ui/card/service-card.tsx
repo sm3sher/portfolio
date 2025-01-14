a6e55798fc9b115050462e0ae876fd6f587702ff
Roman Jumatov <roman.jum99@gmail.com>
2025-01-14 13:02:19 +0100

    style: redesign service card

--- app/ui/card/service-card.tsx
+++ app/ui/card/service-card.tsx
@@ -9,22 +9,25 @@ type Props = {
 
 export default function ServiceCard({ icon, title, tags, description }: Props) {
   return (
-    <div className="group relative h-full overflow-hidden rounded-2xl px-4 py-5 drop-shadow">
+    <div className="group relative h-full overflow-hidden rounded-3xl bg-[--background-color] px-6 py-8 shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-xl dark:bg-[--banner-color]">
       <div className="absolute -left-1/4 top-1/4 h-36 w-[150%] animate-spin-slow bg-[--highlight] opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-80" />
-      <div className="absolute inset-1 rounded-xl bg-[--background-color] dark:bg-[--banner-color]" />
-      <div className="relative">
-        <div className="transition-colors duration-300 ease-in-out group-hover:text-[--highlight]">
+      <div className="absolute inset-1 rounded-3xl bg-[--background-color] dark:bg-[--banner-color]" />
+      <div className="absolute inset-0 rounded-3xl bg-gradient-to-tr from-[--highlight-subtle] to-transparent opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-30" />
+      <div className="relative z-10 flex flex-col items-center text-center">
+        <div className="mb-6 flex h-16 w-16 items-center justify-center rounded-full bg-[--highlight] text-white shadow-md">
           {icon}
-          <h5 className="mb-4 mt-3 md:mb-6 md:mt-4 lg:mb-8 lg:mt-6">{title}</h5>
         </div>
-        <div className="mb-4 flex flex-wrap gap-2 text-xs uppercase">
+        <h5 className="transition-colors duration-300 ease-in-out group-hover:text-[--highlight]">
+          {title}
+        </h5>
+        <div className="my-4 flex flex-wrap justify-center gap-2 text-xs font-medium uppercase text-[--secondary]">
           {tags.map((tag) => (
             <span key={tag} className="base-border rounded-full px-3 py-1">
               {tag}
             </span>
           ))}
         </div>
-        <p className="text-[--secondary] transition-colors duration-300 ease-in-out group-hover:text-[--primary]">
+        <p className="text-sm text-[--secondary] transition-colors duration-300 ease-in-out group-hover:text-[--primary]">
           {description}
         </p>
       </div>
