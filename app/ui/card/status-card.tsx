c83e18b23a7ce7f956bc42b0221e1c26176d7af1
Roman Jumatov <roman.jum99@gmail.com>
2025-01-14 13:06:14 +0100

    style: adjust border radius for status card

--- app/ui/card/status-card.tsx
+++ app/ui/card/status-card.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 export default function StatusCard({ children, icon, title, button }: Props) {
   return (
-    <div className="rounded-2xl bg-[--background-color] px-8 py-6 drop-shadow sm:px-10 sm:py-8 dark:bg-[--banner-color]">
+    <div className="rounded-3xl bg-[--background-color] px-8 py-6 drop-shadow sm:px-10 sm:py-8 dark:bg-[--banner-color]">
       <div className="flex flex-col items-center text-[--highlight]">
         {icon}
         <h5 className="mb-2 mt-4">{title}</h5>
