eb8855585fcbd773c7319d6ba48212f3454173c0
Roman Jumatov <roman.jum99@gmail.com>
2025-01-17 13:21:04 +0100

    test: add tests for form status interactions

--- app/ui/form/contact-form.test.tsx
+++ app/ui/form/contact-form.test.tsx
@@ -8,8 +8,14 @@ import { routing } from '@/i18n/routing';
 import ContactForm from '@/app/ui/form/contact-form';
 import { Form } from '@/app/lib/contentful/generated/sdk';
 
+const FAIL_EMAIL = 'fail@test.com';
+
 vi.mock('@/app/lib/supabase/client', () => {
-  const mockInsert = vi.fn(() => Promise.resolve({ error: null }));
+  const mockInsert = vi.fn((data) =>
+    Promise.resolve(
+      data.email === FAIL_EMAIL ? { error: 'Insert failed' } : { error: null },
+    ),
+  );
   return {
     default: vi.fn(() => ({
       from: vi.fn(() => ({
@@ -26,6 +32,18 @@ const content = {
     role: 'Job title, company name (optional)',
     message: 'Message',
   },
+  successTitle: 'Message Sent!',
+  successButtonLabel: 'Send another Message',
+  errorTitle: 'Something went wrong!',
+  errorButtonLabel: 'Try again',
+};
+
+const validFormData = {
+  name: 'Roman Jumatov',
+  email: 'roman@jumatov.com',
+  role: 'Freelance Software Developer',
+  message: 'Hey there, I have something in mind.',
+  consent: true,
 };
 
 describe('ContactForm', () => {
@@ -45,7 +63,7 @@ describe('ContactForm', () => {
     vi.clearAllMocks();
   });
 
-  it('shows validation errors when fields are empty', async () => {
+  it('shows server validation errors on empty submit', async () => {
     await user.click(screen.getByRole('button'));
 
     screen.getByText(ValidationMessages.nameRequired);
@@ -54,7 +72,17 @@ describe('ContactForm', () => {
     screen.getByText(ValidationMessages.consentRequired);
   });
 
-  it('displays error messages when too much data is entered', async () => {
+  it('shows client validation errors on empty value change', async () => {
+    await fillForm(validFormData);
+    await clearForm();
+
+    screen.getByText(ValidationMessages.nameRequired);
+    screen.getByText(ValidationMessages.emailInvalid);
+    screen.getByText(ValidationMessages.messageRequired);
+    screen.getByText(ValidationMessages.consentRequired);
+  });
+
+  it('shows client validation messages when max length is reached', async () => {
     const formData = {
       name: 'a'.repeat(101),
       email: 'a'.repeat(92).concat('@test.com'),
@@ -62,7 +90,6 @@ describe('ContactForm', () => {
       message: 'A'.repeat(1001),
       consent: false,
     };
-
     await fillForm(formData);
 
     screen.getByText(ValidationMessages.nameMaxLength);
@@ -72,21 +99,59 @@ describe('ContactForm', () => {
   });
 
   it('calls supabase client insert with form data when valid', async () => {
-    const formData = {
-      name: 'Roman Jumatov',
-      email: 'roman@jumatov.com',
-      role: 'Freelance Software Developer',
-      message: 'Hey there, I have something in mind.',
-      consent: true,
-    };
-
-    await fillForm(formData);
-
+    await fillForm(validFormData);
     await user.click(screen.getByRole('button'));
 
     const mockClient = supabaseClient();
     const mockInsert = mockClient.from('contacts').insert;
-    expect(mockInsert).toHaveBeenCalledWith(formData);
+    expect(mockInsert).toHaveBeenNthCalledWith(1, validFormData);
+  });
+
+  it('shows success status card on valid form submission', async () => {
+    await fillForm(validFormData);
+    await user.click(screen.getByRole('button'));
+
+    screen.getByText(content.successTitle);
+  });
+
+  it('clears form on sending another message', async () => {
+    await fillForm(validFormData);
+    await user.click(screen.getByRole('button'));
+
+    await user.click(screen.getByText(content.successButtonLabel));
+
+    expect(
+      screen.getByRole('textbox', { name: content.placeholder.name }),
+    ).toBeEmptyDOMElement();
+    expect(
+      screen.getByRole('textbox', { name: content.placeholder.email }),
+    ).toBeEmptyDOMElement();
+    expect(
+      screen.getByRole('textbox', { name: content.placeholder.role }),
+    ).toBeEmptyDOMElement();
+    expect(
+      screen.getByRole('textbox', { name: content.placeholder.message }),
+    ).toBeEmptyDOMElement();
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+  });
+
+  it('shows error status card on database error', async () => {
+    await fillForm({ ...validFormData, email: FAIL_EMAIL });
+    await user.click(screen.getByRole('button'));
+
+    screen.getByText(content.errorTitle);
+  });
+
+  it('calls supabase client again on retry', async () => {
+    await fillForm({ ...validFormData, email: FAIL_EMAIL });
+    await user.click(screen.getByRole('button'));
+
+    const mockClient = supabaseClient();
+    const mockInsert = mockClient.from('contacts').insert;
+    expect(mockInsert).toHaveBeenCalledOnce();
+
+    await user.click(screen.getByText(content.errorButtonLabel));
+    expect(mockInsert).toHaveBeenCalledTimes(2);
   });
 
   const fillForm = async (formData: ContactFormData) => {
@@ -111,5 +176,21 @@ describe('ContactForm', () => {
     if (formData.consent) {
       await user.click(screen.getByRole('checkbox'));
     }
+  };
+
+  const clearForm = async () => {
+    await user.clear(
+      screen.getByRole('textbox', { name: content.placeholder.name }),
+    );
+    await user.clear(
+      screen.getByRole('textbox', { name: content.placeholder.email }),
+    );
+    await user.clear(
+      screen.getByRole('textbox', { name: content.placeholder.role }),
+    );
+    await user.clear(
+      screen.getByRole('textbox', { name: content.placeholder.message }),
+    );
+    await user.click(screen.getByRole('checkbox', { checked: true }));
   };
 });
