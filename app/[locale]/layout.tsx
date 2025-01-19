ec77a538ade4239fa7d84c4f8f80e06fe20b67c4
Roman Jumatov <roman.jum99@gmail.com>
2025-01-19 23:28:30 +0100

    feat: cache contentful requests for 24 hours

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,20 +3,31 @@ import { ThemeProvider } from 'next-themes';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Locale, routing } from '@/i18n/routing';
-import '@/app/ui/globals.css';
+import { setRequestLocale } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
+import '@/app/ui/globals.css';
 
 type Props = {
   children: ReactNode;
   params: Promise<{ locale: Locale }>;
 };
 
+// TODO: replace with invalidate post request from CMS on publish
+export const revalidate = 86400;
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
 export default async function RootLayout({ children, params }: Props) {
   const { locale } = await params;
   if (!routing.locales.includes(locale)) {
     notFound();
   }
+
+  // Enable static rendering
+  setRequestLocale(locale);
 
   return (
     <html lang={locale} suppressHydrationWarning className="scroll-smooth">
