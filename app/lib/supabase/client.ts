4f1b4c3f8202971972fbf397ddb4169cf92d7ab7
Roman Jumatov <roman.jum99@gmail.com>
2025-01-22 23:36:28 +0100

    refactor: usage of supabase secrets in server makes them private

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,10 +1,12 @@
+'use server';
+
 import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/app/lib/supabase/generated/database.types';
 
 const supabaseClient = () =>
   createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
   );
 
 export default supabaseClient;
