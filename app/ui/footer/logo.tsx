7792ab0975f865de70b70f34067c3c2fd60f8ee3
Roman Jumatov <roman.jum99@gmail.com>
2025-02-02 17:34:34 +0100

    style: redesign logo to include highlight color

--- app/ui/footer/logo.tsx
+++ app/ui/footer/logo.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Logo({ title, description }: Props) {
   return (
     <div className="flex items-center gap-3 sm:gap-4">
-      <div className="rounded-[1.15rem] bg-(--primary) text-(--surface-color) md:rounded-[1.25rem] lg:rounded-[1.45rem]">
+      <div className="rounded-[1.15rem] bg-radial-[circle_at_top_left] from-(--highlight) to-black text-white md:rounded-[1.25rem] lg:rounded-[1.45rem]">
         <SourceCodeIcon className="size-11 stroke-2 p-0.5 md:size-12 lg:size-14" />
       </div>
       <div className="flex flex-col">
