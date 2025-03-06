5f65235488ab5b45265dadf710d299e645b8f2f0
Roman Jumatov <roman.jum99@gmail.com>
2025-03-06 18:26:32 +0100

    fix: ensure that hrefs are not just distinguished by color

--- app/ui/link/external-link.tsx
+++ app/ui/link/external-link.tsx
@@ -19,7 +19,7 @@ export default function ExternalLink({
       href={href || ''}
       target="_blank"
       rel="noopener noreferrer"
-      className={`${children ? 'underline-effect inline-flex items-center gap-1' : 'hover:underline hover:underline-offset-4'} hyphens-auto break-words ${highlight && 'text-(--highlight-link)'}`}
+      className={`${children ? 'underline-effect inline-flex items-center gap-1' : 'underline underline-offset-4'} hyphens-auto break-words ${highlight && 'text-(--highlight-link)'}`}
     >
       {children || href}
       {icon && <LinkSquare02Icon size={16} />}
