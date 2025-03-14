1931ed3cfa0c7e04e44fd3b69770d0b30800803e
Roman Jumatov <roman.jum99@gmail.com>
2025-03-14 11:54:16 +0100

    style: remove stagger form animation to make contact section less distracting

--- app/ui/form/contact-form-fields.tsx
+++ app/ui/form/contact-form-fields.tsx
@@ -1,7 +1,6 @@
 import type { SaveMessageStatus } from '@/app/lib/actions';
 import type { Form } from '@/app/lib/contentful/generated/sdk';
 import type { ContactFormData } from '@/app/lib/schemas';
-import StaggerAnimation from '@/app/ui/animation/stagger-animation';
 import SubmitButton from '@/app/ui/button/submit-button';
 import FormGdprCheckbox from '@/app/ui/form/input/form-gdpr-checkbox';
 import FormText from '@/app/ui/form/input/form-text';
@@ -24,63 +23,57 @@ export default function ContactFormFields({
   register,
 }: Props) {
   return (
-    <form action={handleFormAction}>
-      <StaggerAnimation
-        className="space-y-4"
-        direction="fromLeft"
-        translation={10}
-      >
-        <FormText
-          key="name"
-          register={register}
-          name="name"
-          label={content?.labels?.name || ''}
-          defaultValue={state?.rawData?.name || ''}
-          errors={errors}
-          validationMessages={content?.validationMessages}
-        />
-        <FormText
-          key="email"
-          register={register}
-          name="email"
-          label={content?.labels?.email || ''}
-          defaultValue={state?.rawData?.email}
-          errors={errors}
-          validationMessages={content?.validationMessages}
-        />
-        <FormText
-          key="role"
-          register={register}
-          name="role"
-          label={content?.labels?.role || ''}
-          defaultValue={state?.rawData?.role}
-          errors={errors}
-          validationMessages={content?.validationMessages}
-        />
-        <FormTextarea
-          key="projectDetails"
-          register={register}
-          name="projectDetails"
-          label={content?.labels?.projectDetails || ''}
-          defaultValue={state?.rawData?.projectDetails}
-          errors={errors}
-          validationMessages={content?.validationMessages}
-        />
-        <FormGdprCheckbox
-          key="consent"
-          content={{
-            intro: content?.gdprNoticeIntro,
-            linkText: content?.gdprNoticeLinkText,
-            details: content?.gdprNoticeDetails,
-          }}
-          register={register}
-          name="consent"
-          defaultChecked={state?.rawData?.consent}
-          errors={errors}
-          validationMessages={content?.validationMessages}
-        />
-        <SubmitButton key="submit">{content?.sendButtonLabel}</SubmitButton>
-      </StaggerAnimation>
+    <form className="space-y-4" action={handleFormAction}>
+      <FormText
+        key="name"
+        register={register}
+        name="name"
+        label={content?.labels?.name || ''}
+        defaultValue={state?.rawData?.name || ''}
+        errors={errors}
+        validationMessages={content?.validationMessages}
+      />
+      <FormText
+        key="email"
+        register={register}
+        name="email"
+        label={content?.labels?.email || ''}
+        defaultValue={state?.rawData?.email}
+        errors={errors}
+        validationMessages={content?.validationMessages}
+      />
+      <FormText
+        key="role"
+        register={register}
+        name="role"
+        label={content?.labels?.role || ''}
+        defaultValue={state?.rawData?.role}
+        errors={errors}
+        validationMessages={content?.validationMessages}
+      />
+      <FormTextarea
+        key="projectDetails"
+        register={register}
+        name="projectDetails"
+        label={content?.labels?.projectDetails || ''}
+        defaultValue={state?.rawData?.projectDetails}
+        errors={errors}
+        validationMessages={content?.validationMessages}
+      />
+      <FormGdprCheckbox
+        key="consent"
+        content={{
+          intro: content?.gdprNoticeIntro,
+          linkText: content?.gdprNoticeLinkText,
+          details: content?.gdprNoticeDetails,
+        }}
+        register={register}
+        name="consent"
+        defaultChecked={state?.rawData?.consent}
+        errors={errors}
+        validationMessages={content?.validationMessages}
+      />
+      <SubmitButton key="submit">{content?.sendButtonLabel}</SubmitButton>
     </form>
   );
 }
